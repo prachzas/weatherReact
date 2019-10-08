ddcd12f981d913c81be4d26dd386e629291d5f58
unknown <prachzas_sut@hotmail.com>
2019-10-08 15:43:32 +0700

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+class Title extends React.Component{
+    render(){
+        return(
+            <div> 
+                <h1 className="title-container__title">Weather Finder </h1>
+                <h3 className="title-container__subtitle"> Find out temperature , condition and more ...</h3>
+            </div>
+        );
+    }
+}
+
+export default Title;
